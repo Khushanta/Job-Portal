2fa93c029d1f5513f777efc58a5be4efeede4a85
KhushantaKumar <khushantabh09@gmail.com>
2025-07-16 13:10:06 +0530

    Update

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -59,6 +59,6 @@ export const clerkWebhooks = async (req, res) => {
 
     } catch (error) {
         console.log(error.message);
-        res.json({success:false,message:'Webhooks Error'})
+        res.json({success:false, message:'Webhooks Error'})
     }
 }
