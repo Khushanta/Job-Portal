3e21547fabe570cefa2a88a00bfd09b3a4ce8eb4
KhushantaKumar <khushantabh09@gmail.com>
2025-07-16 13:55:24 +0530

    u

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -18,47 +18,50 @@ export const clerkWebhooks = async (req, res) => {
 
         //Switch Cases for different Events
         switch (type) {
-            case 'user.created':{
+            case 'user.created': {
 
                 const userData = {
-                    _id:data.id,
+                    _id: data.id,
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    image: data-image_url,
+                    image: data.image_url,
                     resume: ''
 
                 }
                 await User.create(userData)
                 res.json({})
                 break;
-                
+
             }
-            case 'user.updated':{
+            case 'user.updated': {
 
                 const userData = {
                     email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
-                    image: data-image_url,
+                    image: data - image_url,
                 }
                 await User.findByIdAndUpdate(data.id, userData)
                 res.json({})
                 break;
 
             }
-            case 'user.deleted':{
-                await User.findByIdAndDeleted(data.id)
+            case 'user.deleted': {
+                await User.findByIdAndDelete(data.id)
                 res.json({})
                 break;
 
             }
-
+            
             default:
+                res.status(400).json({ message: "Unhandled webhook event" });
+
+
                 break;
         }
 
 
     } catch (error) {
         console.log(error.message);
-        res.json({success:false, message:'Webhooks Error'})
+        res.json({ success: false, message: 'Webhooks Error' })
     }
 }
